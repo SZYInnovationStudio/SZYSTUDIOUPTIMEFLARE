3ff009851c0ca92045519c3cb399a0843d5fbd1e
ShiZhongyan🍭 <159118303+shizhongyangh@users.noreply.github.com>
2025-12-03 22:28:24 +0800

    Update footer text to include new links and message

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import { pageConfig } from '@/uptime.config'
 
 export default function Footer() {
   const defaultFooter =
-    '<p style="text-align: center; font-size: 12px; margin-top: 10px;"> Open-source monitoring and status page powered by <a href="https://github.com/lyc8503/UptimeFlare" target="_blank">Uptimeflare</a>, made with ❤ by <a href="https://github.com/lyc8503" target="_blank">lyc8503</a>. </p>'
+    '<p style="text-align: center; font-size: 12px; margin-top: 10px;"> 托管于 <a href="https://www.szystudio.cn" target="_blank">SZY创新工作室</a>,  <a href="https://www.szyd.fun" target="_blank">ShiZhongyan🍭</a>制作</p>'
 
   return (
     <>
